993800ceda47173bd914b97ab19053fc4d430d06
Hessel de Jong <info@hesseldejong.com>
2025-01-20 15:42:46 +0100

    Build

--- dashboard-widget/build/index.asset.php
+++ dashboard-widget/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '63177570ceade742d656');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '9de1f1445c0f6edc3373');
