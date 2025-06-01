507f0146ca4a6be128f790454fa1337f08373dbb
Rogier Lankhorst <rogierlankhorst@gmail.com>
2025-06-01 16:44:25 +0200

    build

--- src/Admin/Dashboard_Widget/build/index.asset.php
+++ src/Admin/Dashboard_Widget/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => 'afcc264f7dfa9a289957');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => 'bd9d0d7202578a1a8bb5');
