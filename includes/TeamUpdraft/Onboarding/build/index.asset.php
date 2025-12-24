917b4a55821414d6047c8a2595254cb6e1483513
Rogier Lankhorst <rogierlankhorst@gmail.com>
2025-12-24 12:42:07 +0100

    fix onboarding ajax fallback

--- includes/TeamUpdraft/Onboarding/build/index.asset.php
+++ includes/TeamUpdraft/Onboarding/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '4def4bbf7e348c1a5d69');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '22e064770fe0564b53df');
