e96f8d4ac684751109638d1527d3163b552d93f3
Rogier Lankhorst <rogierlankhorst@gmail.com>
2026-06-30 13:38:14 +0200

    sync to core

---
+++ includes/Admin/App/build/index.05adeb15a5cc9b7f728f.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '05adeb15a5cc9b7f728f');
